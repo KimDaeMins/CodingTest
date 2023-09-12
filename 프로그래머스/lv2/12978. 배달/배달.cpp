1da9206192a8ee6ac8594854a76c8053c28cc4f0
KimDaeMins <68540137+KimDaeMins@users.noreply.github.com>
2023-09-12 09:55:07 +0900

    [level 2] Title: 배달, Time: 0.09 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12978. 배달/배달.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+vector<vector<int>> graph;
+vector<long> check;
+void DFS(int now, long nowDis)
+{
+    check[now] = nowDis;
+
+    for (int i = 0; i < graph.size(); ++i)
+    {
+        if (graph[now][i] != 0 && check[i] > nowDis + graph[now][i])
+        {
+            DFS(i, nowDis + graph[now][i]);
+        }
+    }
+    return;
+}
+int solution(int N, vector<vector<int> > road, int K) {
+    int answer = 0;
+
+    for (int i = 0; i < N; ++i)
+    {
+        vector<int> a;
+        a.resize(N, 500000);
+        graph.push_back(a);
+    }
+    check.resize(N, K + 1);
+
+    for (int i = 0; i < road.size(); ++i)
+    {
+        int minLoad = min(graph[road[i][0] - 1][road[i][1] - 1], road[i][2]);
+        graph[road[i][0] - 1][road[i][1] - 1] = minLoad;
+        graph[road[i][1] - 1][road[i][0] - 1] = minLoad;
+    }
+
+    DFS(0, 0);
+
+    for (int i = 0; i < N; ++i)
+    {
+        if (check[i] <= K)
+            answer++;
+    }
+    return answer;
+}
